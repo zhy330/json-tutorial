ef7502c1aa58a1e63ebab1c05d72fcffb1b0a981
zhy <123.qq.com>
2024-09-03 19:43:38 +0800

    修改了练习1

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -20,6 +20,11 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
         return LEPT_PARSE_INVALID_VALUE;
     c->json += 3;
+    lept_parse_whitespace(c);
+    if (*c->json != '\0') {
+        v->type = LEPT_NULL;
+        return LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
     v->type = LEPT_NULL;
     return LEPT_PARSE_OK;
 }
@@ -27,6 +32,8 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
         case 'n':  return lept_parse_null(c, v);
+        case 'f':  return;
+        case 't':  return;
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
         default:   return LEPT_PARSE_INVALID_VALUE;
     }
