b6aa2b46c36470a02dae4c95735f426286a8bc0b
zhy <123.qq.com>
2024-09-04 22:24:44 +0800

    完成3-1

--- tutorial03/leptjson.c
+++ tutorial03/leptjson.c
@@ -154,11 +154,14 @@ lept_type lept_get_type(const lept_value* v) {
 
 int lept_get_boolean(const lept_value* v) {
     /* \TODO */
-    return 0;
+    assert(v != NULL && (v->type == LEPT_TRUE || v->type == LEPT_FALSE));
+    return v->type == LEPT_TRUE;
 }
 
 void lept_set_boolean(lept_value* v, int b) {
     /* \TODO */
+    lept_free(v);
+    v->type = b ? LEPT_TRUE : LEPT_FALSE;
 }
 
 double lept_get_number(const lept_value* v) {
@@ -168,6 +171,9 @@ double lept_get_number(const lept_value* v) {
 
 void lept_set_number(lept_value* v, double n) {
     /* \TODO */
+    lept_free(v);
+    v->u.n = n;
+    v->type = LEPT_NUMBER;
 }
 
 const char* lept_get_string(const lept_value* v) {
--- tutorial03/test.c
+++ tutorial03/test.c
@@ -190,10 +190,24 @@ static void test_access_null() {
 static void test_access_boolean() {
     /* \TODO */
     /* Use EXPECT_TRUE() and EXPECT_FALSE() */
+    lept_value v;
+    lept_init(&v);
+    lept_set_boolean(&v, 0);
+    EXPECT_FALSE(lept_get_boolean(&v));
+    lept_set_boolean(&v, 7);
+    EXPECT_TRUE(lept_get_boolean(&v));
+    lept_set_boolean(&v, -2);
+    EXPECT_TRUE(lept_get_boolean(&v));
+    lept_free(&v);
 }
 
 static void test_access_number() {
     /* \TODO */
+    lept_value v;
+    lept_init(&v);
+    lept_set_number(&v, 78.0);
+    EXPECT_EQ_DOUBLE(78.0, lept_get_number(&v));
+    lept_free(&v);
 }
 
 static void test_access_string() {
